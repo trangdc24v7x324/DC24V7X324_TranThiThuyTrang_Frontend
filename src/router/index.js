88885fed2c4bda5c6a520975c482b399aa64f9e2
Tran Thi Thuy Trang <trangdc24v7x324@dttx.ctu.edu.vn>
2025-11-16 20:49:47 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
     name: "contactbook",
     component: ContactBook,
   },
+  {
+    path: "/:pathMatch(.*)*", // bắt mọi đường dẫn không khớp
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="notfound-page text-center mt-5">
+    <h1>404 - Không tìm thấy trang</h1>
+    <router-link to="/">Quay về trang chủ</router-link>
+  </div>
+</template>
+
+<style scoped>
+.notfound-page {
+  padding: 100px 0;
+  font-size: 1.5rem;
+}
+</style>
